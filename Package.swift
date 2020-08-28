7a3a86dd7ea7598e5bfb27de6f3feaa341d9f904
Taylor Holliday <taylor@audulus.com>
2020-08-28 14:18:33 -0700

    Specify platforms.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MetalNanoVG",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v8)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
